b33cedd5711ce7bd43ae96ad5988d608767de0ba
MTG2000 <mtg0987654321@gmail.com>
2025-10-07 13:37:51 +0400

    testing tokens

--- examples/with-import-map/app/src/App.tsx
+++ examples/with-import-map/app/src/App.tsx
@@ -3,11 +3,17 @@ import { css, cx } from '#ui-lib/css'
 import { Stack } from '#ui-lib/jsx'
 import { button } from '#ui-lib/recipes'
 import { Button } from 'panda-mono-with-import-map-ui-lib'
+import { token } from '#ui-lib/tokens'
 
 function App() {
   return (
     <>
-      <h1 className={css({ fontSize: '4xl', lineHeight: '1.1' })}>Hello from Panda 🐼</h1>
+      <h1 className={css({ 
+          fontSize: '4xl', 
+          lineHeight: '1.1', 
+          border: `1px solid ${token('colors.amber.300')}`, // This doesn't work
+          // border: `1px solid token(colors.amber.500)` // This works
+       })}>Hello from Panda 🐼</h1>
       <h2 className={css({ fontSize: '2xl', my: 4, color: 'yellow.400' })}>module-resolution/with-import-map</h2>
       <Stack>
         <Button visual="funky" shape="square" size="lg">
